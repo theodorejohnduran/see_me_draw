db7fb2310701bf582c190235c8a89ab0fe2afa68
mikoypft <54622861+mikoypft@users.noreply.github.com>
2020-02-19 03:23:59 +0800

    burat

--- app/src/main/java/com/example/seemedraw1/EasyOne.java
+++ app/src/main/java/com/example/seemedraw1/EasyOne.java
@@ -11,7 +11,8 @@ public class EasyOne extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_easy_one);
+        PaintView paintView = new PaintView(this);
+        setContentView(paintView);
 
         ConstraintLayout constraintLayout = findViewById(R.id.layout);
         AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
